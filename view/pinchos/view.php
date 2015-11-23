af361da9a1536bff8cffae2419456c35a5c1e025
khvilaboa <khvilaboa@esei.uvigo.es>
2015-11-23 13:16:56 +0100

    Cambio de diseño en la vista de ver pinchos

--- view/pinchos/view.php
+++ view/pinchos/view.php
@@ -12,25 +12,28 @@ $view->setVariable("title", "View Post"); ?>
 
 
 <div>
-	<h1><?= $pincho->getNombrePincho(); ?></h1>
-	<img src="">
-	<ul>
-		<li>Precio: <?= $pincho->getPrecioPincho(); ?><br></li>
-		<li>Id: <?= $pincho->getIdPincho(); ?><br></li>
-		<li>Nombre: <?= $pincho->getNombrePincho(); ?><br></li>
-		<li>Descripci&oacute;n: <?= $pincho->getDescripcionPincho(); ?><br></li>
-		<li>Email: <?= $pincho->getEmailPincho(); ?><br></li>
-		<li>Aprobado: <?= $pincho->getAprobadaPincho(); ?><br></li>
-		<li>Fotografia:<br></li>
-		<img src=<?php if($pincho->getFotoPincho()!=NULL){
-							echo "img/pinchos/".$pincho->getFotoPincho();
-						} else {
-							echo "img/pinchos/default.png";
-						}?> alt="Foto de pincho" class="user-img img-circle">
-	</ul>
+	<div class="pincho-view-title"><h1><?= $pincho->getNombrePincho(); ?></h1></div>
+	<div class="pincho-view-img"><img src="<?= 'img/pinchos/'.$pincho->getFotoPincho(); ?>"></div>
+	<div class="pincho-view-description">
 
+		<table class="table table-striped table-bordered">
+			<tr>
+				<th>Descripci&oacute;n</th>
+				<td><?= $pincho->getDescripcionPincho(); ?></td>
+			</tr>
+			<tr>
+				<th>Precio</th>
+				<td><?= $pincho->getPrecioPincho(); ?></td>
+			</tr>
+			<tr>
+				<th>Contacto</th>
+				<td><?= $pincho->getEmailPincho(); ?></td>
+			</tr>
+		</table>
+	</div>
 	<?php if(isset($_SESSION["type"]) && $_SESSION["type"] == 0): ?>
-		<a href="index.php?controller=pinchos&action=aprobar&id=<?= $pincho->getIdPincho(); ?>" class="btn btn-default <?= (($pincho->getAprobadaPincho() == 1)?"disabled":"") ?>" role="button"><?= (($pincho->getAprobadaPincho() == 1)?"Aceptada":"Aceptar") ?></a>
+		<div class="pincho-view-confirm">
+			<a href="index.php?controller=pinchos&action=aprobar&id=<?= $pincho->getIdPincho(); ?>" class="btn btn-default <?= (($pincho->getAprobadaPincho() == 1)?"disabled":"") ?>" role="button"><?= (($pincho->getAprobadaPincho() == 1)?"Aceptada":"Aceptar") ?></a>
+		</div>
 	<?php endif; ?>
-
 </div>
