0bd96d91786bf13ed219c0c17c038c183f2cc803
khvilaboa <khvilaboa@esei.uvigo.es>
2015-11-25 17:43:41 +0100

    Sidebar responsive

--- view/layouts/default.php
+++ view/layouts/default.php
@@ -8,6 +8,52 @@
 	$view = ViewManager::getInstance();
 	$currentuser = $view->getVariable("currentusername");
 	$concurso = (new ConcursoMapper())->getInfo();
+
+function getNavItems($phone) {
+	$concurso = (new ConcursoMapper())->getInfo();
+	$started = $concurso->isStarted();
+
+	$items = '<li class="nav-pill"><a href="index.php?controller=concurso&amp;action=view">Concurso</a></li>';
+	if($started) {
+		$items .= '<li class="nav-pill"><a href="index.php?controller=pinchos&amp;action=listar">Pinchos</a></li>';
+	}
+	if (!isset($_SESSION["user"])) {
+		if($started) {
+			$items .= '<li class="nav-pill"><a href="index.php?controller=juradoprofesional&amp;action=index">Jurado Profesional</a></li>';
+		}
+
+		$items .= '<li class="nav-pill"><a href="index.php?controller=usuarios&amp;action=login">'. (($phone)?'<span class="glyphicon glyphicon-log-in"></span> ':'') . 'Identificarse</a></li>';
+
+		if($started) {
+			$items .= '<li class="nav-pill"><a href="index.php?controller=usuarios&amp;action=register">'. (($phone)?'<span class="glyphicon glyphicon-user"></span> ':'') . 'Registrarse</a></li>';
+		} else {
+			$items .= '<li class="nav-pill"><a href="index.php?controller=usuarios&amp;action=register">'. (($phone)?'<span class="glyphicon glyphicon-user"></span> ':'') . 'Registrar establecimiento</a></li>';
+		}
+
+	} else {
+		if($_SESSION["type"]==Usuario::ORGANIZADOR and !$started) {
+			$items .= '<li><a href="index.php?controller=juradoprofesional&amp;action=index">Jurado Profesional</a></li>';
+		} else if($_SESSION["type"]==Usuario::JURADO_POPULAR) {
+			$items .= '<li><a href="index.php?controller=pinchos&amp;action=listarPinchosUsuario">Mis pinchos</a></li><li><a href="index.php?controller=codigos&amp;action=introducir">Introducir Código</a></li>';
+		} else if($_SESSION["type"]==Usuario::ESTABLECIMIENTO) {
+			if($started) {
+				$items = '<li><a href="index.php?controller=pinchos&amp;action=presentar">Propuesta</a></li>';
+			}
+
+			if((new PinchoMapper())->getPinchoValidado($_SESSION["user"])<>-1) {
+				$items = '<li><a href="index.php?controller=codigos&amp;action=generar">Generar c&oacute;digos</a></li>';
+			}
+		}
+
+		if($started) {
+			$items .= '<li class="nav-pill"><a href="index.php?controller=juradoprofesional&amp;action=index">Jurado Profesional</a></li>';
+		}
+
+		$items .= '<li><a href="index.php?controller=usuarios&amp;action=logout">Desconectar <?= $currentuser ?></a></li>';
+
+	}
+	return $items;
+}
 ?>
 
 <!DOCTYPE html>
@@ -51,6 +97,22 @@
             ?>
         </div>
 
+		<nav class="navbar navbar-inverse visible-xs">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">WebSiteName</a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav"> <?= getNavItems(true); ?> </ul>
+				</div>
+			</div>
+		</nav>
+
 
 		<div class="row" id="banner">
 			<div id="title">Chopin</div>
@@ -59,62 +121,19 @@
 		<div class="container">
 
 			<div class="row">
-				<div class="col-xs-3">
+				<div class="col-sm-3 hidden-xs">
 					<div id="sidebar">
 						<ul class="nav nav-pills nav-stacked" role="tablist">
-							<li class="nav-pill"><a href="index.php?controller=concurso&amp;action=view">Concurso</a></li>
-							<?php if($concurso->isStarted()): ?>
-								<li class="nav-pill"><a href="index.php?controller=pinchos&amp;action=listar">Pinchos</a></li>
-							<?php endif; ?> 
-							<?php if (!isset($currentuser)): ?>
-                                <?php if($concurso->isStarted()): ?>
-                                    <li class="nav-pill"><a href="index.php?controller=juradoprofesional&amp;action=index">Jurado Profesional</a></li>
-                                <?php endif; ?>
-
-								<li class="nav-pill"><a href="index.php?controller=usuarios&amp;action=login">Identificarse</a></li>
-								
-								<?php if($concurso->isStarted()): ?>
-									<li class="nav-pill"><a href="index.php?controller=usuarios&amp;action=register">Registrarse</a></li>
-								<?php else: ?> 
-									<li class="nav-pill"><a href="index.php?controller=usuarios&amp;action=register">Registrar establecimiento</a></li>
-								<?php endif; ?> 
-							<?php else: ?>
-								<?php if($_SESSION["type"]==Usuario::ORGANIZADOR and !$concurso->isStarted()): ?>
-						            <li><a href="index.php?controller=juradoprofesional&amp;action=index">Jurado Profesional</a></li>
-                                <?php endif; ?>
-
-                                <?php if($_SESSION["type"]==Usuario::JURADO_POPULAR): ?>
-						            <li><a href="index.php?controller=pinchos&amp;action=listarPinchosUsuario">Mis pinchos</a></li>
-									<li><a href="index.php?controller=codigos&amp;action=introducir">Introducir Código</a></li>
-                                <?php endif; ?>
-
-                                <?php if($_SESSION["type"]==Usuario::ESTABLECIMIENTO): ?>
-                                    <?php if(!$concurso->isStarted()): ?>
-						                <li><a href="index.php?controller=pinchos&amp;action=presentar">Propuesta</a></li>
-						            <?php endif; ?>
-
-									<?php if((new PinchoMapper())->getPinchoValidado($_SESSION["user"])<>-1): ?>
-										<li><a href="index.php?controller=codigos&amp;action=generar">Generar c&oacute;digos</a></li>
-									<?php endif; ?>
-                                <?php endif; ?>
-
-                                <?php if($concurso->isStarted()): ?>
-                                    <li class="nav-pill"><a href="index.php?controller=juradoprofesional&amp;action=index">Jurado Profesional</a></li>
-								<?php endif; ?>
-							
-								<li><a href="index.php?controller=usuarios&amp;action=logout">Desconectar <?= $currentuser ?></a></li>
-							<?php endif ?>
-							       
+							<?= getNavItems(false); ?>
 						</ul>
 					</div>
 				</div>
-				
-				<div class="col-xs-9" id="content">
+
+				<div class="col-xs-12 col-sm-9" id="content">
 					<?= $view->getFragment(ViewManager::DEFAULT_FRAGMENT) ?>
 				</div>
 			</div>
-			
-			
+
 			<div class="row" id="footer">
 				<div class="container text-center">
 					<p class="text-muted">Chopin: <a href="#" data-toggle="tooltip" data-placement="top" title="Hooray!">ABP Project.</a></p>
