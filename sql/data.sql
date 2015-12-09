b8cd1cb152e3b162a0c38ba96c5c865c84fd62c2
khvilaboa <khvilaboa@esei.uvigo.es>
2015-12-09 17:55:54 +0100

    Datos (temporales) para establecimentos
    Modificación de los datos de prueba para adaptarlos a la adicción de
    campos. Pretende evitar los fallos hasta que se pongan unos datos de
    prueba más creibles y amigables.

--- sql/data.sql
+++ sql/data.sql
@@ -10,14 +10,14 @@ INSERT INTO usuario(email, password, fotoperfil, telefono, tipo, preferencias) V
 INSERT INTO usuario(email, password, fotoperfil, telefono, tipo, preferencias) VALUES ("establecimiento4@gmail.com", "123456", "default.png", "999999999", 3, "");
 INSERT INTO juradoprofesional(experiencia, email) values ("5 estrellas michelin","jprof@gmail.com");
 INSERT INTO juradoprofesional(experiencia, email) values ("3 estrellas michelin","jprof2@gmail.com");
-INSERT INTO establecimiento(direccion, coordenadas, email) VALUES ("Ourense
-", "(42.33578929999999, -7.863880999999992)", "establecimiento@gmail.com");
-INSERT INTO establecimiento(direccion, coordenadas, email) VALUES ("Ourense
-", "(42.33578929999999, -7.863880999999992)", "establecimiento2@gmail.com");
-INSERT INTO establecimiento(direccion, coordenadas, email) VALUES ("Ourense
-", "(42.33578929999999, -7.863880999999992)", "establecimiento3@gmail.com");
-INSERT INTO establecimiento(direccion, coordenadas, email) VALUES ("Ourense
-", "(42.33578929999999, -7.863880999999992)", "establecimiento4@gmail.com");
+INSERT INTO establecimiento(direccion, coordenadas, email, nombre, horario) VALUES ("Ourense
+", "(42.33578929999999, -7.863880999999992)", "establecimiento@gmail.com", "Establecimiento 1", "10:00-11:00");
+INSERT INTO establecimiento(direccion, coordenadas, email, nombre, horario) VALUES ("Ourense
+", "(42.33578929999999, -7.863880999999992)", "establecimiento2@gmail.com", "Establecimiento 2", "11:00-12:00");
+INSERT INTO establecimiento(direccion, coordenadas, email, nombre, horario) VALUES ("Ourense
+", "(42.33578929999999, -7.863880999999992)", "establecimiento3@gmail.com", "Establecimiento 3", "6:00-8:00");
+INSERT INTO establecimiento(direccion, coordenadas, email, nombre, horario) VALUES ("Ourense
+", "(42.33578929999999, -7.863880999999992)", "establecimiento4@gmail.com", "Establecimiento 4", "9:00-15:00");
 INSERT INTO pincho(precio, idpincho, nombre, descripcion, email, aprobada, foto) VALUES (2.55, 1, "Calamares en su tinta", "Calamares cocinados con su tinta", "establecimiento@gmail.com", 1, "default.png");
 INSERT INTO pincho(precio, idpincho, nombre, descripcion, email, aprobada, foto) VALUES (3.00, 2, "Pincho de tortilla", "Utiliza huevos y patatas", "establecimiento2@gmail.com", 1, "default.png");
 INSERT INTO pincho(precio, idpincho, nombre, descripcion, email, aprobada, foto) VALUES (5.00, 3, "Vegetariano", "Compuesto por vegetales", "establecimiento3@gmail.com", 1, "default.png");
