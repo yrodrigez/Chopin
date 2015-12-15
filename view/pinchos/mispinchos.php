747048aa0c9b37ec4fc0320ec5cb93cabd3e05c2
khvilaboa <khvilaboa@esei.uvigo.es>
2015-12-15 20:26:30 +0100

    Modificación de estilo en lista de pinchos probados

--- view/pinchos/mispinchos.php
+++ view/pinchos/mispinchos.php
@@ -44,25 +44,25 @@ $porcentaje = $totalPinchos > 0 ? round($mispinchosSinRepetir*100/$totalPinchos)
         <?php endif; ?>
     </div>
     <?php foreach ($pinchos as $pincho): ?>
-        <div class="thumbnail">
-            <div class="row row-height">
-                <div class="col-xs-4 col-sm-3 col-height">
-                    <img src='<?= 'img/pinchos/'.$pincho->getFotoPincho();?>' alt='Foto_Pincho' class='user-img img-circle'/>
-                </div>
-                <div class="col-xs-8 col-sm-6 col-height col-middle">
-                    <div class="thumb-username">
-                        Nombre: <?=$pincho->getNombrePincho();?>
-                        <br/>
-                        Ingredientes:
-                        <br/>
-                        <ul class='ingredientes'>
-                            <?php foreach($pincho->getIngredientesPincho() as $ingrediente){ ?>
-                                <li class="ingrediente"><?= $ingrediente; ?></li>
-                            <?php } ?>
-                        </ul>
+        <a href="index.php?controller=pinchos&amp;action=view&amp;id=<?= $pincho->getIdPincho()?>">
+            <div class="thumbnail">
+                <div class="row row-height">
+                    <div class="col-xs-3 col-sm-3 col-sm-offset-2 col-height">
+                        <img src=<?php if($pincho->getFotoPincho()!=NULL){
+                            echo "img/pinchos/".$pincho->getFotoPincho();
+                        } else {
+                            echo "img/pinchos/default.png";
+                        }?> alt="Foto Pincho" class="user-img img-circle">
+                    </div>
+                    <div class="col-xs-5 col-sm-5 col-height col-middle">
+                        <div class=""><?=$pincho->getNombrePincho()?></div>
+                    </div>
+
+                    <div class="col-xs-4 col-sm-2 col-height col-middle">
+                        <div class=""><?=$pincho->getPrecioPincho()?>€</div>
                     </div>
                 </div>
             </div>
-        </div>
+        </a>
     <?php endforeach; ?>
 </div>
